58880ad852ec1e5546ec2ae3f079928f2bae509b
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-29 00:00:26 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) == 0) {
+        basic.showIcon(IconNames.No)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    } else if (input.acceleration(Dimension.X) > 20) {
+        basic.showArrow(ArrowNames.East)
+    } else if (input.acceleration(Dimension.X) < -20) {
+        basic.showArrow(ArrowNames.West)
+    }
+    if (input.acceleration(Dimension.Y) == 0) {
+        basic.showIcon(IconNames.No)
+    } else if (input.acceleration(Dimension.X) > 20) {
+        basic.showArrow(ArrowNames.South)
+    } else if (input.acceleration(Dimension.X) < -20) {
+        basic.showArrow(ArrowNames.North)
+    }
+})
